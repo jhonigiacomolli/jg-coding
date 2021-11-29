5eef0e3d61fb7e0b343874e4c09a26dbacf71435
Jhoni <edicao@microsite.net.br>
2021-11-29 06:29:45 -0300

    fixed bug in mobile menu z-index

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -78,7 +78,6 @@ const HeaderContainer = styled.header`
     align-items: center;
     justify-content: center;
     position: relative;
-    z-index: 1;
 
     @media(max-width: 767px) {
       grid-template-columns: 1fr 1fr;
